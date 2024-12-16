3380d94b4e550ce126ae1f87316dc1e9eda63bef
Lornwri <34406133+Lornwri@users.noreply.github.com>
2024-12-16 00:05:31 -0600

    New changes for Azure upload

--- models/index.js
+++ models/index.js
@@ -1,13 +1,25 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const createStudentModel = require('./student'); // Adjust path as needed
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite', // Path to your SQLite database
-});
+// Load environment variables (e.g., DB credentials, Azure configs)
 const env = process.env.NODE_ENV || 'development';
 
-const dbPassword = process.env.DB_PASSWORD;
+const sequelize = new Sequelize(
+    process.env.DB_NAME || 'student-sign-in-database ',         // Database name
+    process.env.DB_USER || 'natashaw',         // Username
+    process.env.DB_PASSWORD, '%QNy^5r7sCpRZE*d8AeY'   // Password
+    {
+        host: process.env.DB_HOST || 'student-sign-in-database-server-natashaw.windows.net'// Hostname
+        dialect: 'mssql',
+        dialectOptions: {
+            options: {
+                encrypt: true,
+                trustServerCertificate: false, // Disable only if necessary for local testing
+            }
+        }
+        logging: false, // Disable SQL query logging in production
+
+); //code help by Jetbrains IDE
 
 // Create the Student model
 const Student = createStudentModel(sequelize, DataTypes);
